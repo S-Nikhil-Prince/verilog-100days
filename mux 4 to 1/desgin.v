aa02530ca344e98896c34d2cd751efba6029dd7a
Nikhil Prince Sodadasi <166209449+S-Nikhil-Prince@users.noreply.github.com>
2025-03-26 14:18:37 +0530

    Create desgin.v

---
+++ mux 4 to 1/desgin.v
@@ -0,0 +1,17 @@
+// 4:1 MUX 
+module mux(s,a,f);
+  input [3:0]a;
+  input [1:0]s;
+  output reg f;
+  always @(s or a)
+    begin 
+      case(s)
+        2'b00: f=a[0];
+        2'b01: f=a[1];
+        2'b10: f=a[2];
+        2'b11: f=a[3];
+        default: f=1'bx;
+      endcase
+    end
+endmodule 
+    
