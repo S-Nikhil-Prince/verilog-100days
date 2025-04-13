42e49c007c7333c74a6863c90f12721afd07f95f
Nikitha <nikithasodadasi07@gmail.com>
2025-04-13 18:11:10 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
    On branch main
     Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   Day 5/Mux 2-1/Images/Mux 2-1-page-00001.jpg
    	new file:   Day 5/Mux 2-1/Images/Mux 2-1-page-00002.jpg
    	new file:   Day 5/Mux 2-1/Images/Mux 2-1-page-00003.jpg
    	new file:   Day 5/Mux 2-1/Images/Mux 2-1.pdf
    	modified:   Day 5/Mux 2-1/desgin.v
    	modified:   Day 5/Mux 2-1/test.v

--- Day 5/Mux 2-1/desgin.v
+++ Day 5/Mux 2-1/desgin.v
@@ -1,1 +1,12 @@
-//Mux 2:1 Design
+//Design code
+module mux(input [1:0]a,input s,output reg y);
+  always@(s or a) begin
+    case(s)
+      1'b0: y=a[0];
+      1'b1: y=a[1];
+      default: y=1'bx;
+    endcase
+  end
+endmodule
+      
+    
--- Day 5/Mux 2-1/test.v
+++ Day 5/Mux 2-1/test.v
@@ -1,1 +1,13 @@
-//2:1 MUX Testbench
+//test bench
+module tb_mux;
+  reg [1:0]a;
+  reg s;
+  wire y;
+  mux u1(.y(y),.s(s),.a(a));
+  initial begin
+    $monitor("s=%0b,y=%0b",s,y);
+    a=2'b10;
+    s=0;#10;
+    s=1;
+  end
+endmodule
